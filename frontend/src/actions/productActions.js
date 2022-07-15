0ed000f9aea5e79cecad8823e9fcad65f36e8ebc
Cotamor <cotamor@gmail.com>
2022-07-15 16:17:32 +0900

    Add productDetailsReducer

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,23 +1,41 @@
 import axios from 'axios'
 import {
-  PRODUCT_LIST_FAIL,
-  PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants'
-
-const API_URL = '/api/products/'
 
 // Thunk make it possible to create function inside a function
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST })
 
-    const { data } = await axios.get(API_URL)
+    const { data } = await axios.get('/api/products')
 
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+export const productDetails = (productId) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST })
+
+    const { data } = await axios.get(`/api/products/${productId}`)
+
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
