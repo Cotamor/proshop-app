121ac313a2a5bb400b3ab5a960fad29068c78ad9
Cotamor <cotamor@gmail.com>
2022-07-16 12:37:56 +0900

    Car Screen

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link, useParams, useLocation } from 'react-router-dom'
+import { Link, useParams, useLocation, useNavigate } from 'react-router-dom'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
@@ -8,13 +8,13 @@ import { addToCart } from '../actions/cartActions'
 
 const CartScreen = () => {
   const { cartItems } = useSelector((state) => state.cart)
-  console.log(cartItems)
 
   const params = useParams()
   const productId = params.id
   const location = useLocation()
   const qty = Number(new URLSearchParams(location.search).get('qty'))
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   useEffect(() => {
     if (productId) {
@@ -22,10 +22,95 @@ const CartScreen = () => {
     }
   }, [dispatch, productId, qty])
 
+  const removeFromCartHandler = (id) => {
+    console.log('remove')
+  }
+
+  const checkoutHandler = () => {
+    console.log('checkout')
+    navigate('/login?redirect=shipping')
+  }
+
   return (
-    <div>
-      id:{productId} qty:{qty}
-    </div>
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your car is empty<Link to='/'>Go back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as='select'
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={3}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      onClick={removeFromCartHandler(item.product)}
+                    >
+                      <i className='fas fa-trash'></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.price * item.qty, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+          </ListGroup>
+          <ListGroup.Item>
+            <div className='d-grid'>
+              <Button
+                type='button'
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              >
+                Proceed To Checkout
+              </Button>
+            </div>
+          </ListGroup.Item>
+        </Card>
+      </Col>
+    </Row>
   )
 }
 export default CartScreen
