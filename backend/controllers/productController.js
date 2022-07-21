69a31dfddc34e2d0ff0e91ce6555fe5daef81e47
Cotamor <cotamor@gmail.com>
2022-07-21 23:35:11 +0900

    Create & Update Product Endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -14,7 +14,7 @@ const getProducts = asyncHandler(async (req, res) => {
 // @desc    Fetch single product
 // @route   GET /api/products/:id
 // @access  Public
-const getProductById =  asyncHandler(async (req, res) => {
+const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id)
 
   if (product) {
@@ -28,16 +28,61 @@ const getProductById =  asyncHandler(async (req, res) => {
 // @desc    Delete product
 // @route   DELETE /api/products/:id
 // @access  Public / Admin
-const deleteProduct =  asyncHandler(async (req, res) => {
+const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id)
 
   if (product) {
     await product.remove()
-    res.status(201).json({message: 'Product removed'})
+    res.status(201).json({ message: 'Product removed' })
+  } else {
+    res.status(404)
+    throw new Error('Product Not Found')
+  }
+})
+// @desc    Create product
+// @route   POST /api/products
+// @access  Public / Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    user: req.user._id,
+    name: 'Sample name',
+    price: 0,
+    description: 'Sample description',
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+  })
+
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
+})
+
+// @desc    Update product
+// @route   PUT /api/products/:id
+// @access  Public / Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image = image
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
   } else {
     res.status(404)
     throw new Error('Product Not Found')
   }
 })
 
-export { getProducts, getProductById, deleteProduct }
+export { getProducts, getProductById, deleteProduct, createProduct, updateProduct }
